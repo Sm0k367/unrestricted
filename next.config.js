d0832bc1114dc74fa20a60d585f9f8f3c71468d8
Sm0k367 <139561920+Sm0k367@users.noreply.github.com>
2025-06-02 22:15:29 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'cdn.openai.com',
+      'oaidalleapiprodscus.blob.core.windows.net',
+      'replicate.delivery',
+      'pbxt.replicate.delivery'
+    ],
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+}
+
+module.exports = nextConfig
